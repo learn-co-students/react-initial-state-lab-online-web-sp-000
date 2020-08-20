5e01b289bf358b2016d5f0414f8e0a99b9202e55
batsaikhan84 <u.batsaikhan@gmail.com>
2020-08-20 15:38:38 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {secondsLeft: props.initialCount}
+    }
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            this.setState({
+                secondsLeft: this.state.secondsLeft - 1
+            })
+        }, 1000)
+    }
+    countReturn = () => {
+        if (this.state.secondsLeft !== 0) {
+            return <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+        } else {
+            return <div>Boom!</div>
+        }
+    }
+    render() {
+        return (
+            this.countReturn()
+        )
+    }
+
+}
