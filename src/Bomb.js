952004f14fc79cadfe76609747ec3e49c7fb584e
github username <github email address>
2020-09-02 22:29:07 +0100

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+import { ProgressPlugin } from 'webpack';
+
+class Bomb extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    } 
+
+    render(){
+        return(
+            <p>{ this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`}</p>
+        )
+    }
+
+
+}
+
+export default Bomb;
