9978ce189430cfe8ed0e461c91a2184c9cce0f9e
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-18 18:42:58 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ImageSlider from './ImageSlider';
 import Bomb from './Bomb';
 
 ReactDOM.render(
-  <div>
-    <ImageSlider />
-    <Bomb initialCount={120} />
-  </div>,
+  <div><ImageSlider /><Bomb initialCount={120} /></div>,
   document.getElementById('root')
 );
