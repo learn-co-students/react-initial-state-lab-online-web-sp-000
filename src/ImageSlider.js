908aa5641b1baec4ba09704c9a58705d2c789811
jenleebeaver <jen.lee.beaver@gmail.com>
2021-01-19 11:38:51 -0800

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,11 +1,21 @@
 
 import React, { Component } from 'react';
+import Bomb from './Bomb.js';
 
 export default class ImageSlider extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
     render(){
         return (
-            <div></div>
+            <div>
+                 I am on slide {this.state.currentSlideIndex}
+            </div>
         )
     }
 
