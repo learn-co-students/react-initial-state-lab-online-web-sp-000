836de6a5cbb87762756611cec96fd6828557e3aa
Scott Hoisington <hoyzington@gmail.com>
2021-03-13 20:36:46 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -8,7 +8,7 @@ export default class Bomb extends Component {
   }
 
   render() {
-    if (this.state.secondsLeft == 0) {
+    if (this.state.secondsLeft === 0) {
       return 'Boom!'
     } else {
       return `${this.state.secondsLeft} seconds left before I go boom!`
