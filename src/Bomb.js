60f9af429b1b7fad803463e0746f8e166d811d02
keelsk <keelsk@gmail.com>
2021-03-18 15:10:38 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,15 @@
 // your Bomb code here!
+import { render } from "enzyme"
+import React from "react"
+
+export default class Bomb extends React.Component{
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    render(){
+        return this.state.secondsLeft===0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+    }
+}
