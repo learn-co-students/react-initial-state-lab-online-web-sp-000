85881e66b94714c9f72005fb7d7f916d433fd891
TrueC-D <aletheacary@gmail.com>
2021-06-12 19:45:00 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,40 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    boomStateText = () =>{
+        let boomState = this.state.secondsLeft
+        if(boomState> 0){
+           return  `${this.state.secondsLeft} seconds left before I go boom!`
+        } else {
+        return `Boom!`
+        }
+    }
+
+    // countDown(){
+    //     let newCount = this.state.secondsLeft - 1
+    //     this.setState({
+    //         secondsLeft: newCount
+    //     })
+
+    // }
+
+    render(){
+        // setTimeout(countDown, 1000)
+        return(
+        <div>
+            {this.boomStateText()}
+        </div>
+        )
+    }
+
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+    constructor(){
+        super()
+        this.state = {currentSlideIndex: 0}
+    }
+
+    render(){
+        // thisText = () =>{`I am on slide ${this.state.currentSlideIndex}`}
+        return(
+            <div>
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+
+}
+
+export default ImageSlider;
